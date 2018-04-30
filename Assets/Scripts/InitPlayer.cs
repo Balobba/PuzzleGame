42649b8c3f70eb2c73d5ffba2084aec221c0c4b5
Oscar Magnusson <balobba@gmail.com>
2018-04-30 19:42:47 +0200

    Add lost scripts. Add new palette

---
+++ Assets/Scripts/InitPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitPlayer : MonoBehaviour { //Initiates the player by spawning it to a specific spawn point
+
+    private static bool playerExists;
+
+    public Transform spawnPoint;
+
+    private void Awake()
+    {
+
+        gameObject.transform.position = spawnPoint.position;
+
+    }
+
+
+}
---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour { //Controlls the fade animations (when warping)
+
+    Animator anim;
+    public bool isFading = false;
+
+	void Start () {
+        anim = GetComponent<Animator> ();
+
+    }
+
+    public IEnumerator FadeToClear()
+    {
+        isFading = true;
+        anim.SetTrigger("FadeIn");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+        
+        
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        isFading = true;
+        anim.SetTrigger("FadeOut");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+        
+    }
+
+    void AnimationComplete() //This function is called at the end of the animation
+    {
+        isFading = false;
+
+    }
+
+}
